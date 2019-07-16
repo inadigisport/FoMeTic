6d9c56b633d29d5c504ef553e4280b276a73e94f
Faris <mfarisazka@gmail.com>
2019-07-16 10:32:18 +0800

    add class for export data

---
+++ app/src/main/java/com/example/fometic/FileUtils.java
@@ -0,0 +1,33 @@
+package com.example.fometic;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.os.Environment;
+
+import java.io.File;
+
+public class FileUtils {
+
+
+    public static File createDirIfNotExist(String path){
+        File dir = new File(path);
+        if( !dir.exists() ){
+            dir.mkdir();
+        }
+        return dir;
+    }
+
+    /* Checks if external storage is available for read and write */
+    public static boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
+    }
+
+    /* Checks if external storage is available to at least read */
+    public static boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state);
+    }
+}
